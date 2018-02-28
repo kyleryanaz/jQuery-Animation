a88ad460354e7c146eed254f18d30de5e8d398b5
Kyle <kyleryan480@gmail.com>
2018-02-28 14:30:39 -0700

    chicken moves itself

--- script.js
+++ script.js
@@ -3,37 +3,38 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
-//Moves the chicken on click
-$("#chicken").click(function() {
+function moveChicken() {
   let direction = getRandomInt(4);
   switch (direction) {
     case 0:
       console.log("up");
-      $(this)
+      $("#chicken")
         .animate({ bottom: "+=1vh" })
         .animate({ bottom: "+=20vh" })
         .animate({ bottom: "-=1vh" });
       break;
     case 1:
       console.log("right");
-      $(this)
+      $("#chicken")
         .animate({ bottom: "+=1vh" })
         .animate({ left: "+=20vh" })
         .animate({ bottom: "-=1vh" });
       break;
     case 2:
       console.log("down");
-      $(this)
+      $("#chicken")
         .animate({ bottom: "+=1vh" })
         .animate({ bottom: "-=20vh" })
         .animate({ bottom: "-=1vh" });
       break;
     case 3:
       console.log("left");
-      $(this)
+      $("#chicken")
         .animate({ bottom: "+=1vh" })
         .animate({ left: "-=20vh" })
         .animate({ bottom: "-=1vh" });
       break;
   }
-});
+}
+
+setInterval(moveChicken, 3000);
