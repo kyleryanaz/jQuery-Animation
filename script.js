7759019e155b231f7cf1cda44bfde730040fe51b
Kyle <kyleryan480@gmail.com>
2018-03-01 00:05:05 -0700

    + pig, + fields

--- script.js
+++ script.js
@@ -49,4 +49,51 @@ function moveChicken() {
   }
 }
 
+function movePig() {
+  let direction = getRandomInt(4);
+  switch (direction) {
+    case 0:
+      console.log("up");
+      $("#pig")
+        .animate({ bottom: "+=1vh" }, 50)
+        .animate({ bottom: "+=5vh" }, 50)
+        .animate({ bottom: "-=1vh" }, 50)
+        .animate({ bottom: "+=1vh" }, 50)
+        .animate({ bottom: "+=5vh" }, 50)
+        .animate({ bottom: "-=1vh" }, 50);
+      break;
+    case 1:
+      console.log("right");
+      $("#pig")
+        .animate({ bottom: "+=1vh" }, 50)
+        .animate({ left: "+=5vh" }, 50)
+        .animate({ bottom: "-=1vh" }, 50)
+        .animate({ bottom: "+=1vh" }, 50)
+        .animate({ left: "+=5vh" }, 50)
+        .animate({ bottom: "-=1vh" }, 50);
+      break;
+    case 2:
+      console.log("down");
+      $("#pig")
+        .animate({ bottom: "+=1vh" }, 50)
+        .animate({ bottom: "-=5vh" }, 50)
+        .animate({ bottom: "-=1vh" }, 50)
+        .animate({ bottom: "+=1vh" }, 50)
+        .animate({ bottom: "-=5vh" }, 50)
+        .animate({ bottom: "-=1vh" }, 50);
+      break;
+    case 3:
+      console.log("left");
+      $("#pig")
+        .animate({ bottom: "+=1vh" }, 50)
+        .animate({ left: "-=5vh" }, 50)
+        .animate({ bottom: "-=1vh" }, 50)
+        .animate({ bottom: "+=1vh" }, 50)
+        .animate({ left: "-=5vh" }, 50)
+        .animate({ bottom: "-=1vh" }, 50);
+      break;
+  }
+}
+
 setInterval(moveChicken, 3000);
+setInterval(movePig, 5000);
